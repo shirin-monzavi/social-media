724a0591d4b9f0125c08e263b1540c77539d8961
Shirin <shirin.monzavi.1372@gmail.com>
2025-08-27 13:29:43 +0330

    social media app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+
+import appName  from "./includes/variable";
 
 function App() {
+  console.log(appName);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+          <h1>{appName}</h1>
+          here my content goes in.
     </div>
   );
 }
---
+++ src/includes/variable.js
@@ -0,0 +1,3 @@
+const appName='Social Media';
+
+export default appName;
