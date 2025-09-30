56d344a4ad9204bc8ed3c388f4d84da70f8af230
Shirin <shirin.monzavi.1372@gmail.com>
2025-09-30 11:32:47 +0330

    add description

--- src/components/Forms/index.js
+++ src/components/Forms/index.js
@@ -1,26 +1,43 @@
+import { setSelectionRange } from "@testing-library/user-event/dist/utils";
 import { useState } from "react";
 
 export default function Form() {
 
     const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
 
     const handle1 = (e) => {
         e.preventDefault();
         console.log({ title })
+                console.log({ description })
     }
-    return (<>
+    return (
         <form onSubmit={handle1}>
-            <input type="text"
-                value={title}
-                onChange={(e) => { setTitle(e.target.value) }}
-                placeholder="Enter the title"
-                maxLength={5}
-            />
+            <br />
             <div>
-                {title}
+                <label htmlFor="title">
+                    Title:
+                    <input
+                        id="title"
+                        type="text"
+                        value={title}
+                        onChange={(e) => { setTitle(e.target.value) }}
+                        placeholder="Enter the title"
+                        maxLength={5}
+                    />
+                </label>
+            </div>
+            <div>
+                <label>
+                    Description:
+                    <textarea placeholder="Enter description"
+                        maxLength={20}
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
+                </label>
             </div>
             <button>Add Post</button>
         </form>
-
-    </>);
+    );
 }
