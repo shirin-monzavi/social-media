fd766455d9231eb0af6f12f1c82a18a4f7ec3c77
monzavi.shirin <monzavi.shirin@GPA001P2102485>
2025-10-06 10:49:47 +0330

    add use ref for function components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from "./components/Header";
 import Posts from "./components/Posts";
 import Form from './components/Forms'
 import ClassCounter from './playground/CounterClass/CounterClass'
+import ClassFunction from './playground/CounterFunction/CounterFunction.js'
 
 export default function App() {
   return (
     <>
       <Header />
       <ClassCounter />
+      <ClassFunction />
       <Posts />
       <Form />
       <Footer />
--- src/playground/CounterFunction/CounterFunction.js
+++ src/playground/CounterFunction/CounterFunction.js
@@ -1,7 +1,19 @@
+import { useRef } from 'react';
+
 export default function CounterFunction(props) {
     const { title, agree } = props;
+    const inputRef = useRef();
+    const handleClick = () => {
+        inputRef.current.focus();
+        inputRef.current.value = "Ok!";
 
+    }
     return (
-        <p>{title} {agree ? 'Yes' : 'NO'}</p>
+        <>
+            <p>{title} {agree ? 'Yes' : 'NO'}</p>
+            <input type="text" placeholder="Please focus" ref={inputRef} />
+            <button type="button" onClick={handleClick}>Press button</button>
+        </>
+
     )
 }
