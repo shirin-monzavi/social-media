9826fd6cbfaae14b491025691407790c04568a75
monzavi.shirin <monzavi.shirin@GPA001P2102485>
2025-10-06 10:33:20 +0330

    add create ref for dom elements

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Posts from "./components/Posts";
 import Form from './components/Forms'
+import ClassCounter from './playground/CounterClass/CounterClass'
 
 export default function App() {
   return (
     <>
       <Header />
+      <ClassCounter />
       <Posts />
       <Form />
       <Footer />
--- src/playground/CounterClass/CounterClass.js
+++ src/playground/CounterClass/CounterClass.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createRef } from 'react';
 class CounterClass extends React.Component {
     constructor() {
         super();
@@ -6,23 +6,35 @@ class CounterClass extends React.Component {
             counter: 1,
             status: "none"
         }
+        this.inputRefIncrease = createRef();
+        this.inputRefDecrease = createRef();
     }
-
 
     render() {
 
         const handleIncrease = () => {
-            this.setState({ counter: this.state.counter + 1, status:"Increased"});
+            this.setState({ counter: this.state.counter + 1, status: "Increased" });
+            this.inputRefIncrease.current.focus();
         }
 
         const handleDecreasee = () => {
-            this.setState({ counter: this.state.counter - 1 , status:"Decreased"});
+            this.setState({ counter: this.state.counter - 1, status: "Decreased" });
+            this.inputRefDecrease.current.focus();
         }
 
         return <>
             <p>Counter {this.state.counter}</p>
-            <p>Status {this.state.status}</p>
+            <p>Last Action: {this.state.status}</p>
+            <input type="text"
+                placeholder='Focous on increase'
+                ref={this.inputRefIncrease}
+            />
             <button onClick={handleIncrease}>Increase</button>
+
+            <input type="text"
+                placeholder='Focous on decrease'
+                ref={this.inputRefDecrease}
+            />
             <button onClick={handleDecreasee}>Decrease</button>
         </>
     }
