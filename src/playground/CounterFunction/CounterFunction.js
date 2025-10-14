f38b98bc121f0cf34ce2f95900fd995e071af4a1
Shirin <shirin.monzavi.1372@gmail.com>
2025-10-14 15:00:02 +0330

    add use effect

--- src/playground/CounterFunction/CounterFunction.js
+++ src/playground/CounterFunction/CounterFunction.js
@@ -1,20 +1,67 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import Prime from "./../prime.js";
 
 export default function CounterFunction(props) {
     const { title, agree } = props;
+    const [userName, setUserName] = useState('Ali')
+    const [family, setFamily] = useState()
+    const [count, setCount] = useState(0)
     const inputRef = useRef();
+
+    // const [primeNumer, setPrimeNum] = useState(null)
+    // const [maxPrimeNumer, setMaxPrimeNumner] = useState(null)
+    // const [isCalculating, setIsCalaculate] = useState(true)
+
+
     const handleClick = () => {
         inputRef.current.focus();
         inputRef.current.value = "Ok!";
+        setUserName('Mahdi')
 
     }
-    console.log('Function component rendered');
+
+    useEffect(() => {
+        console.log("mount");
+        let counter = 0;
+
+        var timer = setInterval(() => {
+            console.log('h',counter)
+        }, 10)
+        return  ()=>{clearInterval(timer)};
+    }, [])
+
+    const handleFamily = () => {
+
+        setFamily('Akbari');
+        setCount(count + 1)
+    }
+
+    useEffect(() => {
+        console.log('count' + count);
+
+        return () => {
+            console.log('unmount')
+        }
+    }, [count])
+
+
 
     return (
         <>
+            {/* {isCalculating && (<p><strong>We are calculating please wait ...</strong></p>)}
+
+            {maxPrimeNumer !== null && (<div>
+                <p> The max number is {maxPrimeNumer}</p>
+                <p> The max prime number is {primeNumer}</p>
+            </div>)} */}
+
             <p>{title} for {props.userName}  {agree ? 'Yes' : 'NO'}</p>
+            <p>{userName}</p>
+            <p>{userName} {count}</p>
             <input type="text" placeholder="Please focus" ref={inputRef} />
             <button type="button" onClick={handleClick}>Press button</button>
+            <button type="button" onClick={handleFamily}>Change family</button>
+
         </>
 
     )
