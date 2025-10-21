87f9302cd26d29c7e492293265af0a1e5206c00b
monzavi.shirin <monzavi.shirin@GPA001P2102485>
2025-10-21 09:09:56 +0330

    add react icon

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,10 @@
 import appName from './../../includes/variable.js'
+import { FaBloggerB } from 'react-icons/fa';
+
 export default function Header() {
     return (
         <header>
+            <FaBloggerB />
             <h1>{appName}</h1>
         </header>
     );
