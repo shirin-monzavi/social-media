754c337e279b264ea380c623950acf6537d7761f
monzavi.shirin <monzavi.shirin@GPA001P2102485>
2025-10-21 09:45:28 +0330

    add string interpolation in mixin

--- src/components/Posts/Post/index.js
+++ src/components/Posts/Post/index.js
@@ -17,9 +17,9 @@ export default function Post({ id, title, description, picture, promote, categor
             <h3 >{title}</h3>
             <img src={picture} alt={title} width={100} />
             <p className='post-text'>{description}</p>
-            <p>Category: {getCategory(category)}</p>
+            <p className='p-p'>Category: {getCategory(category)}</p>
             <p className={promoteStyle}>Promote: <strong> {promote ? 'Yes' : 'No'}</strong></p>
-            <p>Status: {getStatus(status)}</p>
+            <p className='p-a'>Status: {getStatus(status)}</p>
             <p>Like Counter {like} <button type="button" onClick={handleLike}>Like</button></p>
             <p className={displikeStyle}
                 style={{ fontSize: 10 + dislike }}
