98cab8f0e143958a5423246a052fb4173cfafecd
monzavi.shirin <monzavi.shirin@GPA001P2102485>
2025-10-21 09:10:41 +0330

    Change icon

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,10 @@
 import appName from './../../includes/variable.js'
-import { FaBloggerB } from 'react-icons/fa';
+import { PiGithubLogoFill } from 'react-icons/pi';
 
 export default function Header() {
     return (
         <header>
-            <FaBloggerB />
+            <PiGithubLogoFill />
             <h1>{appName}</h1>
         </header>
     );
