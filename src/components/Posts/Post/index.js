93eff411624474575a0ef09088a4eb1cedfa4174
Shirin <shirin.monzavi.1372@gmail.com>
2025-10-25 19:32:47 +0330

    add styles in posts

--- src/components/Posts/Post/index.js
+++ src/components/Posts/Post/index.js
@@ -15,16 +15,19 @@ export default function Post({ id, title, description, picture, promote, categor
     return (
         <div className='post-component'>
             <h3 >{title}</h3>
-            <img src={picture} alt={title} width={100} />
-            <p className='post-text'>{description}</p>
-            <p className='p-p'>Category: {getCategory(category)}</p>
-            <p className={promoteStyle}>Promote: <strong> {promote ? 'Yes' : 'No'}</strong></p>
-            <p className='p-a'>Status: {getStatus(status)}</p>
-            <p>Like Counter {like} <button type="button" onClick={handleLike}>Like</button></p>
-            <p className={displikeStyle}
-                style={{ fontSize: 10 + dislike }}
-
-            >DisLike Counter {dislike} <button type="button" onClick={handleDisLike}>DisLike</button></p>
+            <div className='description'>
+                <img src={picture} alt={title} />
+                <span className='post-text'>{description}</span>
+            </div>
+            <div className='info'>
+                <div>Category: <strong>{getCategory(category)}</strong></div>
+                <div>Status: <strong>{getStatus(status)}</strong></div>
+                <div className={promoteStyle}>Promote: <strong> {promote ? 'Yes' : 'No'}</strong></div>
+            </div>
+            <div>
+                <button title='like' type="button" className='like' onClick={handleLike}>{like}</button>
+                <button className='dislike' type="button" title='dislike' onClick={handleDisLike}>{dislike}</button>
+            </div>
         </div>
     )
 }
