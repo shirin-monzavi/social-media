24f166ea94979c32d60d4d0e9aa4dffd6fb2f377
monzavi.shirin <monzavi.shirin@GPA001P2102485>
2025-10-26 12:04:42 +0330

    add style for footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,7 @@
+import './style.scss'
 export default function Footer(){
     return (
-        <footer>
+        <footer className="main">
             Copyright &copy; Shirin Monzavi
         </footer>
     )
