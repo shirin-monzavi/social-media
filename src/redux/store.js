c89ea68a7e726e7802d47e2d5dfdf79fe1cf8612
Shirin <shirin.monzavi.1372@gmail.com>
2025-10-27 18:54:45 +0330

    add reducer for store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import settingsReducer from './settingsSlide'
 
 export const store = configureStore({
-  reducer: {},
-})
+    reducer: { settingsReducer },
+});
+
+console.log('store',settingsReducer)
