27cbef70a535555678151115d33adc30d7b68e9d
monzavi.shirin <monzavi.shirin@GPA001P2102485>
2025-11-02 09:46:37 +0330

    Add route

--- src/App.js
+++ src/App.js
@@ -7,16 +7,19 @@ import PostList from "./components/pages/PostList";
 import Preference from "./components/pages/Preference";
 import AboutUs from "./components/pages/AboutUs";
 import HomePage from "./components/pages/HomePage";
+import { Route, Routes } from "react-router-dom";
 
 export default function App() {
 
   return (
     <>
       <Header />
-      <HomePage />
-      <PostList />
-      <Preference />
-      <AboutUs />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/posts" element={<PostList />}></Route>
+        <Route path="/preferences" element={<Preference />}></Route>
+        <Route path="/about-us" element={<AboutUs />}></Route>
+      </Routes>
       <Footer />
     </>
   );
--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
 import App from './App';
-import { store } from  './redux/store'
+import { store } from './redux/store'
 
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
