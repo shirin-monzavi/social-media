5dae42285e7c6463baf2da6c4f157b66d6b5eea6
Shirin <shirin.monzavi.1372@gmail.com>
2025-11-09 21:56:16 +0330

    add firebase

--- src/database/config.js
+++ src/database/config.js
@@ -1,12 +1,15 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBJaX-8WxxQ4fixah0wwEQD0hBVnXGQVxc",
-  authDomain: "socialmedia-dd077.firebaseapp.com",
-  projectId: "socialmedia-dd077",
-  storageBucket: "socialmedia-dd077.firebasestorage.app",
-  messagingSenderId: "466481795383",
-  appId: "1:466481795383:web:4e325c3f6dfd43a263cab2"
+    apiKey: "AIzaSyBJaX-8WxxQ4fixah0wwEQD0hBVnXGQVxc",
+    authDomain: "socialmedia-dd077.firebaseapp.com",
+    projectId: "socialmedia-dd077",
+    storageBucket: "socialmedia-dd077.firebasestorage.app",
+    messagingSenderId: "466481795383",
+    appId: "1:466481795383:web:4e325c3f6dfd43a263cab2"
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
