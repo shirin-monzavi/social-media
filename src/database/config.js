f222cc4ceaf17e8db2ff46d0059481df89ada0b3
Shirin <shirin.monzavi.1372@gmail.com>
2025-11-09 21:31:42 +0330

    add firebase

---
+++ src/database/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJaX-8WxxQ4fixah0wwEQD0hBVnXGQVxc",
+  authDomain: "socialmedia-dd077.firebaseapp.com",
+  projectId: "socialmedia-dd077",
+  storageBucket: "socialmedia-dd077.firebasestorage.app",
+  messagingSenderId: "466481795383",
+  appId: "1:466481795383:web:4e325c3f6dfd43a263cab2"
+};
+
+const app = initializeApp(firebaseConfig);
