864ce5fa4e977f97ec5ea0bc1ee2af3a485065ec
monzavi.shirin <monzavi.shirin@GPA001P2102485>
2025-11-09 16:06:36 +0330

    use * as for importing reference

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import PostList from "./components/pages/PostList";
 import Preference from "./components/pages/Preference";
 import AboutUs from "./components/pages/AboutUs";
 import HomePage from "./components/pages/HomePage";
-import { Route, Routes } from "react-router-dom";
+import { data, Route, Routes } from "react-router-dom";
 import NotFoundPage from "./components/pages/NotFoundPage";
 import PostItemPage from "./components/pages/PostItemPage";
 import PostFormPage from "./components/pages/PostFormPage";
@@ -12,15 +12,18 @@ import AboutUsMission from "./components/pages/AboutUs/mission";
 import AboutUsPrivacy from "./components/pages/AboutUs/privacy";
 import AboutUsIntroduction from "./components/pages/AboutUs/Introduction";
 import { useEffect } from "react";
-import { save, update, load as databaseLoad, loadById } from "./database";
+import * as database from './database';
 
 export default function App() {
 
   useEffect(() => {
+    console.log('database', database);
+    database.update();
+    database.load();
     //Load the database;
-    databaseLoad();
-    save();
-    update();
+    // databaseLoad();
+    // save();
+    // update();
   }, [])
 
   return (
