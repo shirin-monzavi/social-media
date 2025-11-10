47eebbd43ab73b1664d9f6e6d30c77e88f37d64b
monzavi.shirin <monzavi.shirin@GPA001P2102485>
2025-11-10 11:40:23 +0330

    read data from firestore

--- src/database/read.js
+++ src/database/read.js
@@ -1,8 +1,28 @@
+import { collection, getDoc, getDocs } from 'firebase/firestore';
+import { db } from './config';
 
 export function load() {
-    console.log('Loading ...')
+    const dbCollection = collection(db, "posts");
+
+    getDocs(dbCollection)
+        .then((querySnapshop) => {
+            querySnapshop.forEach((doc) => {
+                const post = {
+                    ...doc.data(),
+                    id: doc.id
+                };
+                console.log('post', post)
+            })
+        })
+        .catch((e) => {
+            console.log('error', e);
+        });
+    // const querySnapshop = await getDocs(collection(db, "users"));
+    // querySnapshop.forEach((doc) => {
+    //     console.log(`${doc.id} => ${doc.data()}`);
+    // })
 }
 
 export function loadById(id) {
-    console.log('Loading ...',id)
+    console.log('Loading ...', id)
 }
