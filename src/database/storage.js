d6e975607e26159b2fd9121b23fba04d194c43cd
monzavi.shirin <monzavi.shirin@GPA001P2102485>
2025-11-18 10:26:41 +0330

    upload file to storage

--- src/database/storage.js
+++ src/database/storage.js
@@ -4,6 +4,7 @@ import { getStorage, uploadBytes, ref } from 'firebase/storage';
 const storage = getStorage(app);
 
 export async function uploadPicture(file) {
+    console.log('f',file);
     const storageRef = ref(storage, 'some-child');
 
     const snapShot = await uploadBytes(storageRef, file);
