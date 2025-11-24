2d4bd6b54d081e0a8559603f6dcae73646557fd8
Shirin <shirin.monzavi.1372@gmail.com>
2025-11-24 10:47:43 +0330

    add loading component and install axios

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,3 +1,10 @@
-export default function Loading(){
-    return (<div className="loading-component">Loading...</div>)
+import './style.scss';
+import {OrbitProgress} from 'react-loading-indicators'
+
+export default function Loading() {
+    return (
+        <div className="loading-component">
+            <OrbitProgress color="#008080" size="medium" text="" textColor="" />
+            Loading...
+        </div>)
 }
